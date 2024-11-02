e63d94a7e5cd6a62acba2742b06c714a30f2fd7f
Petros-Agoston <49579094+Petros-Agoston@users.noreply.github.com>
2024-11-02 10:45:33 +0200

    Update SlowSlide.js

--- SlowSlide.js
+++ SlowSlide.js
@@ -1,1 +1,28 @@
 
+    document.addEventListener("DOMContentLoaded", () => {
+      // Get all anchor links with href targeting IDs
+      const links = document.querySelectorAll('a[href^="#"]');
+
+      function smoothScrollToTarget(targetId) {
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: "smooth"
+          });
+        }
+      }
+
+      // Attach click event listener to each link
+      links.forEach(link => {
+        link.addEventListener('click', (event) => {
+          event.preventDefault(); // Prevent the default jump to the anchor
+
+          const targetId = link.getAttribute("href");
+          smoothScrollToTarget(targetId);
+        });
+      });
+    });
+
+
+
